178631e83cb8d98815ac5a0428824914ae424eee
Mohammad Rafly <mohammadrafly19@gmail.com>
2024-05-17 11:01:45 +0700

    update head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,7 @@
         <meta name="csrf" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <title>{{ config('app.name') }}</title>
-        @viteReactRefresh 
-        @vite(['resources/js/app.jsx', 'resources/css/app.css'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
     </head>
     <body>
